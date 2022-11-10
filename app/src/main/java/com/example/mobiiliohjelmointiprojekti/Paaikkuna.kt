a5c7c5e1c6cfaaa84cb0c726ddcd6cdf3cf6da8a
OttoH97 <82201999+OttoH97@users.noreply.github.com>
2022-11-10 18:04:35 +0200

    Navigointi Pääikkunalle.

--- app/src/main/java/com/example/mobiiliohjelmointiprojekti/Paaikkuna.kt
+++ app/src/main/java/com/example/mobiiliohjelmointiprojekti/Paaikkuna.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class Paaikkuna : Fragment() {
 
@@ -12,10 +14,19 @@ class Paaikkuna : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_paaikkuna, container, false)
 
-        //Koodia
+        //Navigointi Pääikkunasta
+        val btnOmatSivut = view.findViewById<Button>(R.id.btnOmatSivut)
+        val btnArvostelut = view.findViewById<Button>(R.id.btnArvostelut)
+
+        btnOmatSivut.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_paa_to_omatTiedot)
+        }
+
+        btnArvostelut.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_paa_to_arvostelu)
+        }
 
         return view
     }
